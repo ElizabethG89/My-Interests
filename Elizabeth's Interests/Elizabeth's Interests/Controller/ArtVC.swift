223bd76881a1635a86c4bfe4d4800fe9e540edde
ElizabethG89 <39136864+ElizabethG89@users.noreply.github.com>
2018-05-09 15:55:19 -0400

    Add files via upload
    project

---
+++ Elizabeth's Interests/Elizabeth's Interests/Controller/ArtVC.swift
@@ -0,0 +1,35 @@
+//
+//  ArtVC.swift
+//  Elizabeth's Interests
+//
+//  Created by Student on 5/9/18.
+//  Copyright © 2018 Elizabeth. All rights reserved.
+//
+
+import UIKit
+
+class ArtVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
